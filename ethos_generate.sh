397f682c456392bc84e7a52b86966e79d0569bb9
Alexis Murzeau <amubtdx@gmail.com>
2025-07-03 08:30:18 +0200

    generate ethos tflite for both 128 and 256 MAC/cc

--- ethos_generate.sh
+++ ethos_generate.sh
@@ -5,7 +5,8 @@ set -e
 convert_model() {
     local MODEL_PATH=$1
     local MODEL_PATH_NO_EXT=${MODEL_PATH%.*}
-    vela "$MODEL_PATH" --config "$PWD/config/vela.ini" --accelerator-config ethos-u55-256 --system-config Ethos_U55_High_End_Embedded --memory-mode Sram_Only --optimise Performance --verbose-weights --verbose-cycle-estimate  --verbose-performance --show-cpu-operations > "${MODEL_PATH_NO_EXT}_vela.txt"
+    vela "$MODEL_PATH" --config "$PWD/config/vela.ini" --output-dir ${MODEL_PATH_NO_EXT}_H256 --accelerator-config ethos-u55-256 --system-config Ethos_U55_High_End_Embedded --memory-mode Sram_Only --optimise Performance --verbose-weights --verbose-cycle-estimate  --verbose-performance --show-cpu-operations > "${MODEL_PATH_NO_EXT}_H256_vela.txt"
+    vela "$MODEL_PATH" --config "$PWD/config/vela.ini" --output-dir ${MODEL_PATH_NO_EXT}_H128 --accelerator-config ethos-u55-128 --system-config Ethos_U55_Deep_Embedded --memory-mode Sram_Only --optimise Performance --verbose-weights --verbose-cycle-estimate  --verbose-performance --show-cpu-operations > "${MODEL_PATH_NO_EXT}_H128_vela.txt"
 }
 
 convert_model tmp/denoiser_dns_p_int8.tflite
